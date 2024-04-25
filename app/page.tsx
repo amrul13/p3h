b31624f4ad5beeed1e2256ccd9f6edccb7a1f533
amrul13 <dapurmilky@gmail.com>
2024-04-25 18:24:18 +0800

    initial project

--- app/page.tsx
+++ app/page.tsx
@@ -2,112 +2,111 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <section
+        style={{ backgroundImage: `url(${'https://ppph.id/assets/img/hero-ppph.png'})` }}
+        className="flex flex-col justify-between h-screen bg-cover bg-center text-white"
+      >
+        {/* make logo */}
+        <div className="flex items-center justify-end">
+          <Image className="" src={'https://ppph.id/assets/img/new-lppph.png'} alt="logo" width={225} height={225} />
+          <Image className="mt-[50px]" src={'https://ppph.id/assets/img/bpjph_logo.png'} alt="logo" width={225} height={225} />
+        </div>
+
+        {/* make hero text section */}
+        <div className="grid grid-cols-2">
+          <div></div>
+          <div className="text-center flex flex-col items-center justify-end">
+            <p className="text-3xl font-regular text-slate-700">Perekrutan dan Penempatan</p>
+            <h1 className="text-5xl font-bold text-slate-700 md:text-5">PENDAMPING PROSES PRODUK HALAL</h1>
+          </div>
+        </div>
+
+        <div></div>
+      </section>
+      <main className="bg-white px-10 pt-10">
+        <section>
+          <div className="bg-green-200 rounded-lg p-6 mb-6">
+            <h1 className=" text-2xl font-bold text-slate-700 text-center mb-2">Jadi Pendamping Proses Produk Halal Profesional & Miliki Penghasilan Jutaan Rupiah setiap bulan bersama LPPPH EWI</h1>
+            <h1 className=" font-regular text-slate-700 md:text-5 text-center">Indonesia memiliki lebih dari 60 juta pelaku usaha mikro yang 60% merupakan pelaku usaha sektor makanan & minuman. 17 Oktober 2024 seluruh pelaku usaha yang produknya terkait dengan makanan & minuman wajib memiliki sertifikasi halal dari Badan Penyelenggara Jaminan Produk Halal (BPJPH) Kementerian Agama RI.</h1>
+            <h1 className=" font-regular text-slate-700 md:text-5 text-center">Dibutuhkan Pendamping bagi Pelaku Usaha untuk mendaftarkan produknya agar mendapatkan Sertifikat Halal dari BPJPH & dibutuhkan lebih dari 200.000 Pendamping Proses Produk Halal (PPPH) yang Profesional.</h1>
+          </div>
+        </section>
+        <section>
+          <div className="bg-green-200 rounded-lg p-6 mb-6">
+            <h1 className=" text-2xl font-bold text-slate-700 text-center mb-2">Berapa Besar Penghasilan Seorang PPPH ?</h1>
+            <h1 className=" font-regular text-slate-700 md:text-5 text-center">Bila dalam sebulan Anda berhasil mendampingi pengajuan sertifikat halal dan disetujui oleh Komisi Halal BPJPH sebanyak 20 Sertifikat Halal maka Anda akan mendapatkan penghasilan sebesar :</h1>
+            <div className="bg-blue-200 text-blue-800 text-lg font-medium me-2 my-2 px-2.5 py-0.5 rounded border-blue-400 text-center">Rp. 150.000,- x 20 Sertifikat Halal = Rp. 3.000.000,-</div>
+            <h1 className=" font-regular text-slate-700 md:text-5 text-center">Bagaimana bila Anda berhasil mendapat persetujuan bagi 50 Sertifikat Halal atau 100 Sertifikat Halal per bulannya? Penghasilan Anda dapat mencapai belasan bahkan puluhan juta sebulan. </h1>
+          </div>
+        </section>
+        <section>
+          <div className="bg-green-200 rounded-lg p-6 mb-6">
+            <h1 className=" text-2xl font-bold text-slate-700 text-center mb-2">Apa itu Profesi Pendamping Proses Produk Halal Profesional ?</h1>
+          </div>
+
+          <div className="lg:px-32 md:px-0 mb-6">
+            <div className="p-6 bg-white border border-gray-200 rounded-lg shadow h-[500px]">
+                <iframe className="w-full h-full" src="https://www.youtube-nocookie.com/embed/2V1yJefnLk0?si=HQcmXdUNUP-N6FDc" title="Peluang Profesi P3H"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+            </div>
+          </div>
+
+        </section>
+
+        <section>
+          <div className="bg-green-200 rounded-lg p-6 mb-6">
+            <h1 className=" text-2xl font-bold text-slate-700 text-center mb-2">Daftar Segera dan Dapatkan Fasilitas Penempatan Kerja di SWAKARTA</h1>
+            <h1 className=" font-regular text-slate-700 md:text-5 text-center">Silahkan mengisi Data Anda melalui Form di bawah ini untuk mengikuti Pelatihan PPPH dari LPPPH EWI dan Asesmen Penempatan Kerja di SWAKARTA</h1>
+            <div className="mx-12">
+
+              <div className="bg-amber-100 text-amber-500 font-regular my-2 px-2.5 py-0.5 rounded border-blue-400 text-center">SILAHKAN MEMINTA LINK KODE REFERRAL UNTUK MELAKUKAN PENDAFTARAN PPPH EWI</div>
+            </div>
+
+            
+
+    <form className="" action="#">
+      <div className="w-full space-y-6 p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 ">
+        <h5 className="text-xl font-medium text-gray-900">Data Calon PPPH</h5>
+        <span className="text-sm">Seluruh data akan dikirim ke sistem BPJPH</span>
+        <div className="grid gap-6 mb-6 lg:grid-cols-2">
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Nama Lengkap</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nama " required />
+          </div>
+                  
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">NIK</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="012345678910" required />
+                  </div>
+            
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@gmail.com" required />
+                  </div>
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">No Telepon / Whatsapp</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="082123456789" required />
+                  </div>
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Tempat Lahir</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@gmail.com" required />
+          </div>
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 ">Tanggal Lahir</label>
+              <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="12-12-2012" required />
+          </div>
+        
         </div>
       </div>
+              
+        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+    </form>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+      
+    </>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["ppph.id"],
+  },
+};
 
 export default nextConfig;
